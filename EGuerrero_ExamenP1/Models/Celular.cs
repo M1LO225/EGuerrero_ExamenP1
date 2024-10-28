dff978c9c856fcd598ed672f2abb664acccd43e5
M1LO225 <emilio.guerrero.argoti@udla.edu.ec>
2024-10-28 08:00:51 -0500

    Agregar archivos de proyecto.

---
+++ EGuerrero_ExamenP1/Models/Celular.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EGuerrero_ExamenP1.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [NotNull]
+        [StringLength(50)]
+        public string Modelo { get; set; }
+        [Required]
+        [Range(2020, 2024, ErrorMessage = "El celular debe estar entre el año 2020 al 2024")]
+        public int Año { get; set; }
+        public double Precio { get; set; }
+    }
+}
---
+++ EGuerrero_ExamenP1/Models/GuerreroEmilio.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EGuerrero_ExamenP1.Models
+{
+    public class GuerreroEmilio
+    {
+        [Key]
+        public int IdE { get; set; }
+        [Required]
+        [StringLength(30)]
+        [MaxLength(30, ErrorMessage = "El nombre no puede ser de mas de 30 caracteres")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(30)]
+        [MaxLength(30, ErrorMessage = "El apellido no puede ser de mas de 30 caracteres")]
+        public string Apellido { get; set; }
+        [Display(Name = "Tiene experiencia en este trabajo?")]
+        public bool TieneExperiencia { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+        public double Salario { get; set; }
+        [ForeignKey("Celular")]
+        public int Id { get; set; }
+        public virtual Celular? Celular { get; set; } = null!;
+    }
+}
